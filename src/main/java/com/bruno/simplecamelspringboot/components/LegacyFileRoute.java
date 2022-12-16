8e586044cbd5a6d8165c22d407579d70669e7c3f
Bruno <brunomartins1030@gmail.com>
2022-12-16 14:08:39 -0300

    add inline processors

--- src/main/java/com/bruno/simplecamelspringboot/components/LegacyFileRoute.java
+++ src/main/java/com/bruno/simplecamelspringboot/components/LegacyFileRoute.java
@@ -1,15 +1,24 @@
 package com.bruno.simplecamelspringboot.components;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LegacyFileRoute extends RouteBuilder {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public void configure() throws Exception {
 
         from("file:src/data/input?fileName=inputFile.txt")
                 .routeId("legacyFileMoveRouteId")
+                .process(exchange -> {
+                    String filedata = exchange.getIn().getBody(String.class);
+                    logger.info("This  is the read fileData: " + filedata);
+                })
                 .to("file:src/data/output?fileName=outputFile.txt");
 
     }
--- src/test/java/com/bruno/simplecamelspringboot/LegacyFileRouteTest.java
+++ src/test/java/com/bruno/simplecamelspringboot/LegacyFileRouteTest.java
@@ -27,25 +27,6 @@ public class LegacyFileRouteTest {
     ProducerTemplate producerTemplate;
 
     @Test
-    public void testFileMove() throws Exception {
-
-        // Setup the mock
-        String expectedBody = "This is the input file";
-        mockEndpoint.expectedBodiesReceived(expectedBody);
-        mockEndpoint.expectedMinimumMessageCount(1);
-
-        // Tweak the route definition
-        AdviceWith.adviceWith(context, "legacyFileMoveRouteId", routeBuilder -> {
-            routeBuilder.weaveByToUri("file:*").replace().to(mockEndpoint);
-        });
-
-        // Start the context and validate is mock is asserted
-        context.start();
-        mockEndpoint.assertIsSatisfied();
-
-    }
-
-    @Test
     public void testFileMoveByMockingFromEndpoint() throws Exception {
 
         String expectedBody = "This is input data after mocking the from endpoint";
